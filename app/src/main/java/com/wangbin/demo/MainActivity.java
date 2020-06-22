7dbab45e3a26a5fff93b1d74883cb57a8d37eef0
wangbin <robin.wang@zhan.com>
2020-06-22 17:51:27 +0800

    RxJava2引入和基本使用

--- app/src/main/java/com/wangbin/demo/MainActivity.java
+++ app/src/main/java/com/wangbin/demo/MainActivity.java
@@ -1,14 +1,106 @@
 package com.wangbin.demo;
 
 import androidx.appcompat.app.AppCompatActivity;
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        //  1. 创建被观察者 Observable 对象
+//        Observable<Integer> observable = Observable.create(new ObservableOnSubscribe<Integer>() {
+//            // 2. 在复写的subscribe（）里定义需要发送的事件
+//            @Override
+//            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+//                // 通过 ObservableEmitter类对象产生事件并通知观察者
+//                // ObservableEmitter类介绍
+//                // a. 定义：事件发射器
+//                // b. 作用：定义需要发送的事件 & 向观察者发送事件
+//                emitter.onNext(1);
+//                emitter.onNext(2);
+//                emitter.onNext(3);
+//                emitter.onComplete();
+//            }
+//        });
+//
+//        // 步骤2：创建观察者 Observer 并 定义响应事件行为
+//        Observer<Integer> observer = new Observer<Integer>() {
+//            // 通过复写对应方法来 响应 被观察者
+//            @Override
+//            public void onSubscribe(Disposable d) {
+//                Log.d(TAG, "开始采用subscribe连接");
+//            }
+//
+//            @Override
+//            public void onNext(Integer value) {
+//                Log.d(TAG, String.format("对Next事件 %s 作出响应", value));
+//            }
+//
+//            @Override
+//            public void onError(Throwable e) {
+//                Log.d(TAG, "对Error事件作出响应");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.d(TAG, "对Complete事件作出响应");
+//            }
+//        };
+//        // 步骤3：通过订阅（subscribe）连接观察者和被观察者
+//        observable.subscribe(observer);
+
+        Observable.create(new ObservableOnSubscribe<Integer>() {
+            // 2. 在复写的subscribe（）里定义需要发送的事件
+            @Override
+            public void subscribe(ObservableEmitter<Integer> emitter) throws Exception {
+                // 通过 ObservableEmitter类对象产生事件并通知观察者
+                // ObservableEmitter类介绍
+                // a. 定义：事件发射器
+                // b. 作用：定义需要发送的事件 & 向观察者发送事件
+                emitter.onNext(1);
+                emitter.onNext(2);
+                emitter.onNext(3);
+                emitter.onComplete();
+            }
+        }).subscribe(new Observer<Integer>() {
+            private Disposable mDisposable;
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "开始采用subscribe连接");
+                mDisposable = d;
+            }
+
+            @Override
+            public void onNext(Integer value) {
+                Log.d(TAG, String.format("对Next事件 %s 作出响应", value));
+                if (value == 2) {
+                    // 设置在接收到第二个事件后切断观察者和被观察者的连接
+                    mDisposable.dispose();
+                    Log.d(TAG, "已经切断了连接：" + mDisposable.isDisposed());
+                }
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.d(TAG, "对Error事件作出响应");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "对Complete事件作出响应");
+            }
+        });
     }
 }
